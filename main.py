c9ca3949dca9f292f0b42470f20d803c64cd50f2
anthonyprudhomme <anthony.prudhomme@epfedu.fr>
2019-03-04 10:22:56 -0500

    Added points files and display of graph according to file loaded

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+graph = nx.Graph()
+pointsFile = open("Points/"+sys.argv[1], "r")
+nodes = []
+for index, line in enumerate(pointsFile):
+  pos_x, pos_y = [int(x) for x in line.split()]
+  graph.add_node(index, node_position=(pos_x, pos_y))
+  for node_id in nodes:
+    graph.add_edge(index, node_id)
+  nodes.append(index)
+
+node_position=nx.get_node_attributes(graph,'node_position')
+nx.draw(graph, node_position, with_labels=True, font_weight='bold')
+
+plt.show()
