36b13eed14a7146abd78b96bf5fcae62e5376971
GuillaumeG30 <julien.guerin123@gmail.com>
2019-03-24 23:48:33 -0500

    quelques modifs, hanan notamment

--- kruskal.py
+++ kruskal.py
@@ -1,5 +1,5 @@
 def createAdjMatrix(vertices_number, graph_matrix):
-  
+  ##graph_matrix is of the shape [[node_i, node_j, distance_betweew_node_i_and_node_j], [nodek, node_l...]]
   adjMatrix = []
   
   # create N x N matrix filled with 0 edge weights between all vertices
@@ -19,17 +19,17 @@ def kruskal(vertices_number,graph_matrix):
     # create adj matrix from graph
   adjMatrix = createAdjMatrix(vertices_number, graph_matrix)
   graph_matrix_sorted = copy.deepcopy(graph_matrix)
-  graph_matrix_sorted.sort(key=lambda edge: edge[2])
-  list_of_added_edges = []
+  graph_matrix_sorted.sort(key=lambda edge: edge[2]) # its graph matrix where lines are sorted from the shortest distance between 2 nodes to the longest
+  list_of_added_edges = [] #we build MST by adding nodes 1 by 1
   
-  possible_paths = []
+  possible_paths = [] #list of all nodes linked together forming a "possible path"
   while len(list_of_added_edges) < vertices_number-1:
       smallest_weighted_edge = graph_matrix_sorted.pop(0)
       first_path_index = -1
       second_path_index = -1
       for index, path in enumerate(possible_paths):
           if ((smallest_weighted_edge[0] in path) and (smallest_weighted_edge[1] in path)):
-              ##A cycle has been created
+              ##A cycle has been created, so we dont add this node to list_of_added_edges
           else:
               if (smallest_weighted_edge[0] in path):
                   first_path_index = index
@@ -46,5 +46,5 @@ def kruskal(vertices_number,graph_matrix):
        else:
            #Aucun des 2 noeuds n'appartient à un des possible_path deja existants,
            #on cree donc un nouveau possible path. Ce qui revient a creer un autre sous-graphe non-connecte
-
+           
           
